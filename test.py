b9672fa8eedb8e87c4da0eb88f2da6faa02f501f
Tanner Chell <tanner.chell@gmail.com>
2017-02-21 19:54:32 -0700

    Problem 3
    Most of problem 3 complete but recursion is a problem.

--- test.py
+++ test.py
@@ -2,13 +2,18 @@
 import math
 
 def main():
-	
-	n = input('n = ')
-	gratio = 1.61803398875
-	
-	x = (math.pow(gratio, int(n)) - math.pow((1 - gratio), int(n))) / math.sqrt(5)
-	
-	x = round(x)
-	
-	print(x)
+	number_list = [1, 2, 3, 4, 5, 6]
+	for index in range(len(number_list)):
+		# print(index , number_list)
+
+		check = number_list[index]
+
+		largest = False
+		if not largest:
+			largest = check
+
+		elif check > largest:
+			largest = check
+
+	print(largest)
 main()
